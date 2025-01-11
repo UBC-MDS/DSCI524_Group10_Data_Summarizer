591a5f1a65c949ca8842e24338ef057543e3b3dd
stephqwu <stephanie.wu@alumni.ubc.ca>
2025-01-11 13:41:59 -0800

    rm in summarize_missing_values

--- src/summarease/summarize_missing_values.py
+++ src/summarease/summarize_missing_values.py
@@ -1,8 +1,8 @@
 def summarize_missing_values(dataset, summarize_by="table"):
     """
     Summarize the missing values in the dataset, providing information on the number and percentage of 
-    missing values for each column. If there are missing values, a summary table or a visualization can 
-    be generated depending on the `summarize_by` argument.
+    missing values for each column. Generate a summary table or visualization to show the missing values
+    depending on the `summarize_by` argument.
 
     Parameters:
     -----------
@@ -10,14 +10,14 @@ def summarize_missing_values(dataset, summarize_by="table"):
             The dataset to analyze.
         summarize_by (str): 
             The format for summarizing the missing values. 
-                            Options are "table" (default), "plot" or "mixed". If "table", a summary table is 
+                            Options are "table" (default) or "plot". If "table", a summary table is 
                             generated showing the count and percentage of missing values for each column. 
                             If "plot", a bar plot will be displayed showing the missing values per column 
-                            as a percentage of the total. If "mixed", displays both table and plot.
+                            as a percentage of the total.
 
     Returns:
     -------
-        None: Displays either a table or a plot or both, depending on the `summarize_by` argument.
+        None: Displays either a table or a plot, depending on the `summarize_by` argument.
 
     Notes:
     ------
